fa3670d9fdba9dc9d5ec8b0479de416a1ad92c92
ghazal tarek <A.Ahmed08579@student.aast.edu>
2026-05-02 21:21:42 +0300

    Expose server to all network interfaces (0.0.0.0)

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.use('/api', coreRoutes);
 const startServer = async () => {
     try {
         await connectRedis();
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:5000`);
+        app.listen(5000, "0.0.0.0", () => {
+            console.log("Server running on http://0.0.0.0:5000");
         });
     } catch (error) {
         console.error('Failed to start server:', error);
